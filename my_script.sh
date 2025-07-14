a8c8b3d08920c79aa42c8a970a8b7ce9d0bfd41c
BONNIEdyr <3785987746@qq.com>
2025-07-14 23:30:57 +0800

    Initial commit

---
+++ my_script.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+mkdir -p linux_practice/name
+mkdir -p linux_practice/permission
+
+touch linux_practice/name/file1.txt
+touch linux_practice/name/file2.txt
+
+touch linux_practice/permission/file3.txt
+touch linux_practice/permission/file4.txt
+
+rm -i linux_practice/name/file1.txt
+
+mv linux_practice/name/file2.txt linux_practice/name/show.txt
+
+echo "Hello Linux" > linux_practice/name/show.txt
+echo "内容如下："
+cat linux_practice/name/show.txt
+
+chmod 644 linux_practice/permission/file3.txt
+echo "Changed permissions for file3.txt to -rw-r--r--"
+
+chmod 644 linux_practice/permission/file4.txt
+echo "Changed permissions for file4.txt to -rw-r--r--"
+
