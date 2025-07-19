174f0ec53a99248b2a26cb429b1ab2155d51fdb2
BONNIEdyr <3785987746@qq.com>
2025-07-19 11:12:56 +0800

    cmake作业

---
+++ CMake_Test/build/src/modules/A1/CMakeFiles/A1.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/dongyurong/homework2-1/CMake_Test/src/modules/A1/src/A11.cpp" "src/modules/A1/CMakeFiles/A1.dir/src/A11.cpp.o" "gcc" "src/modules/A1/CMakeFiles/A1.dir/src/A11.cpp.o.d"
+  "/home/dongyurong/homework2-1/CMake_Test/src/modules/A1/src/A12.cpp" "src/modules/A1/CMakeFiles/A1.dir/src/A12.cpp.o" "gcc" "src/modules/A1/CMakeFiles/A1.dir/src/A12.cpp.o.d"
+  "/home/dongyurong/homework2-1/CMake_Test/src/modules/A1/src/A13.cpp" "src/modules/A1/CMakeFiles/A1.dir/src/A13.cpp.o" "gcc" "src/modules/A1/CMakeFiles/A1.dir/src/A13.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
